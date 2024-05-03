1e0399d38f0d54b856967256cd14eddc588ed5f6
Etienne van Tonder <evt@cascadia.com.au>
2024-05-03 13:54:06 +1000

    Fix issues with rollup

--- dist/extension.js
+++ dist/extension.js
@@ -33,21 +33,19 @@ exports.deactivate = exports.activate = void 0;
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 const vscode = __importStar(__webpack_require__(1));
+const HelloWorldPanel_1 = __webpack_require__(2);
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 function activate(context) {
     // Use the console to output diagnostic information (console.log) and errors (console.error)
     // This line of code will only be executed once when your extension is activated
     console.log('Congratulations, your extension "dappforge" is now active!');
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with registerCommand
-    // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('dappforge.helloWorld', () => {
-        // The code you place here will be executed every time your command is executed
-        // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World from dAppForge!');
-    });
-    context.subscriptions.push(disposable);
+    context.subscriptions.push(vscode.commands.registerCommand("dappforge.helloWorld", () => {
+        //vscode.window.showInformationMessage(
+        //"token value is: " + TokenManager.getToken()
+        //);
+        HelloWorldPanel_1.HelloWorldPanel.createOrShow(context.extensionUri);
+    }));
 }
 exports.activate = activate;
 // This method is called when your extension is deactivated
@@ -60,6 +58,188 @@ exports.deactivate = deactivate;
 /***/ ((module) => {
 
 module.exports = require("vscode");
+
+/***/ }),
+/* 2 */
+/***/ (function(__unused_webpack_module, exports, __webpack_require__) {
+
+
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+Object.defineProperty(exports, "__esModule", ({ value: true }));
+exports.HelloWorldPanel = void 0;
+const vscode = __importStar(__webpack_require__(1));
+const getNonce_1 = __webpack_require__(3);
+class HelloWorldPanel {
+    /**
+     * Track the currently panel. Only allow a single panel to exist at a time.
+     */
+    static currentPanel;
+    static viewType = "hello-world";
+    _panel;
+    _extensionUri;
+    _disposables = [];
+    static createOrShow(extensionUri) {
+        const column = vscode.window.activeTextEditor
+            ? vscode.window.activeTextEditor.viewColumn
+            : undefined;
+        // If we already have a panel, show it.
+        if (HelloWorldPanel.currentPanel) {
+            HelloWorldPanel.currentPanel._panel.reveal(column);
+            HelloWorldPanel.currentPanel._update();
+            return;
+        }
+        // Otherwise, create a new panel.
+        const panel = vscode.window.createWebviewPanel(HelloWorldPanel.viewType, "HelloWorld", column || vscode.ViewColumn.One, {
+            // Enable javascript in the webview
+            enableScripts: true,
+            // And restrict the webview to only loading content from our extension's `media` directory.
+            localResourceRoots: [
+                vscode.Uri.joinPath(extensionUri, "media"),
+                vscode.Uri.joinPath(extensionUri, "out/compiled"),
+            ],
+        });
+        HelloWorldPanel.currentPanel = new HelloWorldPanel(panel, extensionUri);
+    }
+    static kill() {
+        HelloWorldPanel.currentPanel?.dispose();
+        HelloWorldPanel.currentPanel = undefined;
+    }
+    static revive(panel, extensionUri) {
+        HelloWorldPanel.currentPanel = new HelloWorldPanel(panel, extensionUri);
+    }
+    constructor(panel, extensionUri) {
+        this._panel = panel;
+        this._extensionUri = extensionUri;
+        // Set the webview's initial html content
+        this._update();
+        // Listen for when the panel is disposed
+        // This happens when the user closes the panel or when the panel is closed programatically
+        this._panel.onDidDispose(() => this.dispose(), null, this._disposables);
+        // // Handle messages from the webview
+        // this._panel.webview.onDidReceiveMessage(
+        //   (message) => {
+        //     switch (message.command) {
+        //       case "alert":
+        //         vscode.window.showErrorMessage(message.text);
+        //         return;
+        //     }
+        //   },
+        //   null,
+        //   this._disposables
+        // );
+    }
+    dispose() {
+        HelloWorldPanel.currentPanel = undefined;
+        // Clean up our resources
+        this._panel.dispose();
+        while (this._disposables.length) {
+            const x = this._disposables.pop();
+            if (x) {
+                x.dispose();
+            }
+        }
+    }
+    async _update() {
+        const webview = this._panel.webview;
+        this._panel.webview.html = this._getHtmlForWebview(webview);
+        webview.onDidReceiveMessage(async (data) => {
+            switch (data.type) {
+                case "onInfo": {
+                    if (!data.value) {
+                        return;
+                    }
+                    vscode.window.showInformationMessage(data.value);
+                    break;
+                }
+                case "onError": {
+                    if (!data.value) {
+                        return;
+                    }
+                    vscode.window.showErrorMessage(data.value);
+                    break;
+                }
+                // case "tokens": {
+                //   await Util.globalState.update(accessTokenKey, data.accessToken);
+                //   await Util.globalState.update(refreshTokenKey, data.refreshToken);
+                //   break;
+                // }
+            }
+        });
+    }
+    _getHtmlForWebview(webview) {
+        // // And the uri we use to load this script in the webview
+        const scriptUri = webview.asWebviewUri(vscode.Uri.joinPath(this._extensionUri, "out/compiled", "HelloWorld.js"));
+        // Uri to load styles into webview
+        const stylesResetUri = webview.asWebviewUri(vscode.Uri.joinPath(this._extensionUri, "media", "reset.css"));
+        const stylesMainUri = webview.asWebviewUri(vscode.Uri.joinPath(this._extensionUri, "media", "vscode.css"));
+        // const cssUri = webview.asWebviewUri(
+        //   vscode.Uri.joinPath(this._extensionUri, "out", "compiled/swiper.css")
+        // );
+        // // Use a nonce to only allow specific scripts to be run
+        const nonce = (0, getNonce_1.getNonce)();
+        return `<!DOCTYPE html>
+			<html lang="en">
+			<head>
+				<meta charset="UTF-8">
+				<!--
+					Use a content security policy to only allow loading images from https or from our extension directory,
+					and only allow scripts that have a specific nonce.
+        -->
+        <meta http-equiv="Content-Security-Policy" content="img-src https: data:; style-src 'unsafe-inline' ${webview.cspSource}; script-src 'nonce-${nonce}';">
+				<meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="${stylesResetUri}" rel="stylesheet">
+        <link href="${stylesMainUri}" rel="stylesheet">
+        <script nonce="${nonce}">
+        </script>
+			</head>
+      <body>
+			</body>
+      <script src="${scriptUri}" nonce="${nonce}">
+			</html>`;
+    }
+}
+exports.HelloWorldPanel = HelloWorldPanel;
+
+
+/***/ }),
+/* 3 */
+/***/ ((__unused_webpack_module, exports) => {
+
+
+Object.defineProperty(exports, "__esModule", ({ value: true }));
+exports.getNonce = void 0;
+function getNonce() {
+    let text = "";
+    const possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    for (let i = 0; i < 32; i++) {
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+    return text;
+}
+exports.getNonce = getNonce;
+
 
 /***/ })
 /******/ 	]);
--- rollup.config.js
+++ rollup.config.js
@@ -24,12 +24,10 @@ export default fs
       plugins: [
         svelte({
           // enable run-time checks when not in production
-          dev: !production,
-          // we'll extract any component CSS out into
-          // a separate file - better for performance
-          css: (css) => {
-            css.write(name + ".css");
+          compilerOptions: {
+            dev: !production,
           },
+          emitCss: true,
           preprocess: sveltePreprocess(),
         }),
 
