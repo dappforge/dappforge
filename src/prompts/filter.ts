f98b8e9b43aa06c31126d957c6af655b1242c796
Etienne van Tonder <evt@cascadia.com.au>
2024-06-25 18:26:09 +1000

    Rename filter to .rs

--- src/prompts/filter.ts
+++ src/prompts/filter.ts
@@ -5,7 +5,7 @@ export function isSupported(doc: vscode.TextDocument, aiProvider: string) {
     return (doc.uri.scheme === 'file' || 
         doc.uri.scheme === 'vscode-notebook-cell' ||
         doc.uri.scheme === 'vscode-remote') && 
-        (aiProvider !== 'dAppForge' || (aiProvider === 'dAppForge' && path.extname(doc.uri.fsPath) === ".rust"));
+        (aiProvider !== 'dAppForge' || (aiProvider === 'dAppForge' && path.extname(doc.uri.fsPath) === ".rs"));
 }
 
 export function isNotNeeded(doc: vscode.TextDocument, position: vscode.Position, context: vscode.InlineCompletionContext): boolean {
